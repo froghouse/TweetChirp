c9d326ffde9a902fec9ebfcc90187c23d64ab56e
Tomas <tomas.landberg@gmail.com>
2016-08-07 10:26:07 +0200

    Adding source file to project

---
+++ TweetChirp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ICities;
+using UnityEngine;
+
+namespace TweetChirp
+{
+    public class TweetChirp : IUserMod
+    {
+        public string Description
+        {
+            get
+            {
+                return "Load Twitter messages into Chirper";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Twitter in Chirper";
+            }
+        }
+
+        void OnCreated(IUserMod instance)
+        { }
+    }
+
+    public class LoadingMod : ILoadingExtension
+    {
+        public void OnCreated(ILoading loading) { }
+
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            Debug.Log("test");
+            GameObject template = new GameObject("TweetChirper");
+            GameObject.Instantiate(template);
+        }
+
+        public void OnLevelUnloading() { }
+
+        public void OnReleased() { }
+    }
+}
